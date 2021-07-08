f6404b96bb0e5b5ef6222b5ba845156145719d2c
Eva <halakova.eva@br.ru>
2021-07-08 11:38:10 +0300

    2

---
+++ tamagochi-2.js
@@ -0,0 +1,56 @@
+
+    class tamagochi {
+        name = 'Tim';
+        food = 4;
+        energy = 5;
+        mood = 3;
+        setName (name) {
+            this.name = name
+        };
+        eat(){
+            if(this.food < 5){
+                this.food++ && this.mood--
+            }
+        };
+        sleep(){
+            if(this.energy < 5){
+                this.energy++ && this.food--
+            }
+        };
+        play(){
+            if(this.mood < 5){
+                this.mood++ && this.energy--
+            }
+        };
+        getStatus(){
+            let foodStatus = this.food
+            if(foodStatus < 3){
+                console.log('Я голоден')
+            }else{
+                console.log('Я не голоден')
+            };
+    
+            let energyStatus = this.energy
+            if(energyStatus < 3){
+                console.log('Я хочу спать')
+            }else{
+                console.log('Я не хочу спать')
+            };
+    
+            let moodStatus = this.mood
+            if(moodStatus < 3){
+                console.log('Мне скучно')
+            }else{
+                console.log('Мне весело')
+            };
+    
+            let nameStatus = this.name
+            if( this.food <= 0 || this.energy <= 0 || this.mood){
+                console.log(`${this.name} умер:(`)
+            };
+    
+            console.log(`Имя: ${nameStatus}, Еда: ${foodStatus} (${this.food}), Энергия: ${energyStatus}(${this.energy}), Настроение: ${moodStatus} (${this.mood})`);
+        };
+    };
+        const newTam = new tamagochi()
+    
